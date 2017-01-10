9f63cf089425d75d85caaeb5c3691b4f5ac1941d
Romain-Hamel <romainhamel3d@gmail.com>
2017-01-10 11:31:04 +0100

    base de index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Mon site avec GITHUB</title>
+</head>
+<body>
+	<header>
+		<h1>Mon site avec GITHUB</h1>
+	</header>
+</body>
+</html>
