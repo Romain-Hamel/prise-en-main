427013890bd468038eeb7c9b44034f3dbaa488c9
Romain-Hamel <romainhamel3d@gmail.com>
2017-01-10 12:40:03 +0100

    ajout de menu index.php

--- index.php
+++ index.php
@@ -8,12 +8,26 @@
 <body>
 	<header>
 		<h1>Mon site avec GITHUB</h1>
+		<form >
+			<label for="identifiant">identifiant : </label>
+			<input type="text" name="nom" placeholder="Nom">
+			<input type="text" name="prenom" placeholder="Prénom">
+			<input type="submit" value="Connexion">
+		</form>
 	</header>
 
 	<main>
 		<section>
-			<h2>Ma premiere section</h2>
-			<div>Le contenu changé avec une balise div</div>
+			<h2>Menus</h2>
+			<ul>
+				<li><a href="">Accueil</a></li>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Biographie</a></li>
+				<li><a href="">Newsletter</a></li>
+				<li><a href="">Contact</a></li>
+
+			</ul>
+			
 		</section>
 	</main>
 
